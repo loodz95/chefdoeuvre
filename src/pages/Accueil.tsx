9964149644d2d9eeeddf7b3a18b352fd05e3b334
OZIER Ludovic (SNCF VOYAGEURS / TECHNICENTRE DE PARIS NORD / EQUIPE EXAMENS JOUR) <9001074b@commun.ad.sncf.fr>
2023-01-20 21:01:21 +0100

    changement responsive

--- src/pages/Accueil.tsx
+++ src/pages/Accueil.tsx
@@ -1,5 +1,5 @@
 import SearchBar from "../components/SearchBar";
-
+import MediaQuery from  "react-responsive";
 import React, {useState} from "react"
 import "./Accueil.css"
 import Header from "../components/Header";
@@ -15,14 +15,18 @@ export interface Joueurs {
 
 const Accueil = () =>{
 
-    return (
+    
+
+return (
       <div className="accueil">
         <Header/>
          <NavBar />
         <Bann/>
         <div className="container1">
             <div className="textImage1">
-         <p className ="title">Recherche tes cartes FUT <br /> Favorites et les meilleurs joueurs</p>
+              <MediaQuery maxWidth={1224}> <p className ="title">Recherche tes cartes FUT  Favorites et les meilleurs joueurs</p></MediaQuery>
+              <MediaQuery minWidth={1224}> <p className ="title">Recherche tes cartes FUT <br />  Favorites et les meilleurs joueurs</p></MediaQuery>
+        
           <p className="explanation">Tu cherches des casseurs de reins ? Les meilleurs passeurs ? Sers-toi <br /> de notre outil de recherche pour bâtir une team 
           imbattable en fonction <br /> de tes critères.</p>
          </div>
@@ -32,7 +36,7 @@ const Accueil = () =>{
 
           <div className="container2">
 <div className="textImage2">
-<p className="title2">Sauvegarde toi une team avec les <br /> joueurs que tu as sélectionné</p>
+<p className="title2">Sauvegarde toi une team <br /> avec les  joueurs que tu as sélectionné</p>
 <p className="explanation2">Inscris-toi sur le site et sauvegarde ta wish list de players grâce 
 <br /> à l'outil Fais ta Team.</p>
 </div>
@@ -41,9 +45,11 @@ const Accueil = () =>{
 </div>
 
       <div className="container3">
+        <div className="textImage3">
         <p className="title3">Consulte les joueurs de la semaine</p>
         <p className="explanation3">Qui sont les joueurs qui ont performé cette semaine ? On te liste tout ça !
 </p>
+</div>
              <div className="troisiemeImage">
         </div>
 </div>
