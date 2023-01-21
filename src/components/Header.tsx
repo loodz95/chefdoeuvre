5bf9270e695d2e1899c625db02dda861ad85cd74
OZIER Ludovic (SNCF VOYAGEURS / TECHNICENTRE DE PARIS NORD / EQUIPE EXAMENS JOUR) <9001074b@commun.ad.sncf.fr>
2023-01-21 02:15:33 +0100

    avancee style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,29 @@
 import "./Header.css"
 import ConnexionButton from "./ConnexionButton";
 import SuscribeButton from "./SuscribeButton";
+import { VscAccount} from "react-icons/vsc";
 
 const Header =()=>{
+    const isConnected = false;
 
 return(
 <div className="header">
+    {(isConnected &&(
 <div className="buttonGroup">
     <div className="suscribeButton">
+       
 <SuscribeButton/>
 </div>
 <div >
 <ConnexionButton/>
 </div>
+
 </div>
+ ))||(
+
+ <VscAccount style={{width:2900, height:40, marginTop:7}}    />
+
+ )}
 </div>
     )
 }
